63801846f426ae962b9859ad7b578611a62d56aa
김종원 <nuka5381@ofgod.com>
2026-05-20 16:55:42 +0900

    config/routes.php 일부 수정

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,11 @@
 <?php
   use FFP\Core\Route as Route;
   use FFP\Enums\Route\Method;
-  use FFP\Logger;
 
   Route::append(Method::GET, '/', function (\FFP\App $context, \FFP\DTO\Request $request, \FFP\DTO\Response $response) {
-    Logger::debug(print_r($context, true));
-    Logger::debug(print_r($request, true));
-    Logger::debug(print_r($response, true));
+    \FFP\Logger::debug(print_r($context, true));
+    \FFP\Logger::debug(print_r($request, true));
+    \FFP\Logger::debug(print_r($response, true));
 
     $response->view('index');
   });
